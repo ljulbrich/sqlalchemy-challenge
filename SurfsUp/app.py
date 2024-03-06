fedddc3eb37974ed3930f0cd9c652e3614c9982f
ljulbrich <lucasulbrich@protonmail.com>
2024-03-06 15:39:18 +1100

    Updated app.py

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,33 +1,54 @@
 # Import the dependencies.
 from flask import Flask, jsonify
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
 
 
 #################################################
 # Database Setup
 #################################################
-app = Flask(__name__)
 
 # Create engine using the `hawaii.sqlite` database file
+engine = create_engine('sqlite:///../Resources/hawaii.sqlite')
 
 # Declare a Base using `automap_base()`
-
+Base = automap_base()
 # Use the Base class to reflect the database tables
-
+Base.prepare(autoload_with=engine)
 
 # Assign the measurement class to a variable called `Measurement` and
 # the station class to a variable called `Station`
+Measurement = Base.classes.measurement
 
+Station = Base.classes.station
 
 # Create a session
-
+session = Session(engine)
+conn = engine.connect()
 
 #################################################
 # Flask Setup
 #################################################
-
-
+app = Flask(__name__)
 
 
 #################################################
 # Flask Routes
 #################################################
+@app.rout('/')
+def home():
+    return (
+        f"/api/v1.0/precipitation"
+        f"/api/v1.0/stations"
+        f"/api/v1.0/tobs"
+        f"/api/v1.0/<start-date>"
+        f"/api/v1.0/<start-date>/<end-date>"
+    )
+
+@app.rout("/api/v1.0/precipitation")
+def precipitation():
+    results = session.query(Measurement.)
+
+if __name__ == '__main__':
+    app.run(debug=True)

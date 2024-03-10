5ae75bfb249f5d77508b3d51a67ba3aa4b355704
ljulbrich <lucasulbrich@protonmail.com>
2024-03-10 19:48:37 +1100

    updated app.py

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,17 +1,19 @@
 # Import the dependencies.
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
-from sqlalchemy import text
-
+from pathlib import Path
+import numpy as np
+import regex as re
 
 #################################################
 # Database Setup
 #################################################
 
 # Create engine using the `hawaii.sqlite` database file
-engine = create_engine('sqlite:///../Resources/hawaii.sqlite')
+engine_path = Path('../Resources/hawaii.sqlite')
+engine = create_engine(f'sqlite:///{engine_path}')
 
 # Declare a Base using `automap_base()`
 Base = automap_base()
@@ -38,17 +40,11 @@ app = Flask(__name__)
 #################################################
 # Flask Routes
 #################################################
-@app.rout('/')
+@app.route('/')
 def home():
-    return (
-        f"/api/v1.0/precipitation"
-        f"/api/v1.0/stations"
-        f"/api/v1.0/tobs"
-        f"/api/v1.0/<start-date>"
-        f"/api/v1.0/<start-date>/<end-date>"
-    )
+    return render_template('home.html')
 
-@app.rout("/api/v1.0/precipitation")
+@app.route("/api/v1.0/precipitation")
 def precipitation():
     start_date = "2016-08-23"
     end_date = "2017-08-23"
@@ -58,7 +54,7 @@ def precipitation():
     measurement_list = [{'date':date, 'precipitation':prcp} for date, prcp in results]
     return jsonify(measurement_list)
 
-@app.rout("/api/v1.0/stations")
+@app.route("/api/v1.0/stations")
 def stations():
     session = Session(engine)
     results = session.query(Station.station, Station.name, Station.latitude, Station.longitude, Station.elevation).all()
@@ -67,7 +63,7 @@ def stations():
                      for station, name, latitude, longitude, elevation in results]
     return jsonify(station_list)
 
-@app.rout('/api/v1.0/tobs')
+@app.route('/api/v1.0/tobs')
 def tobs():
     session = Session(engine)
     results = session.query(Measurement.date, Measurement.tobs).filter(Measurement.station == 'USC00519281').all()
@@ -75,8 +71,79 @@ def tobs():
     tobs_list = [{'date':date, 'tobs':tobs} for date, tobs in results]
     return jsonify(tobs_list)
 
-@app.rout('/api/v1.0/<start-date>')
-def 
+@app.route('/api/v1.0/<start_date>')
+def temp_from_date(start_date):
+    start_match = re.match(r"\d{4}\W\d{2}\W\d{2}", start_date)
+    session = Session(engine)
+    dates = session.query(Measurement.date).all()
+    # I have to format the dates from the SQLlite database to check them against the input date
+    # This seems to be faster than the regex
+    blacklist = ['(', "'", ',', ')']
+    date_list = []
+    for date in dates:
+        new_string = ''
+        for char in date:
+            if char in blacklist:
+                new_string += ''
+            else:
+                new_string += char
+        date_list.append(new_string)
+    end_date = '2017-08-23'
+    error_msg_1 = 'Invalid date input.\nPlease use YYYY-MM-DD format.'
+    error_msg_2 = 'Invalid date input.\nPlease input dates between 2010-01-01 and 2017-08-23'
+    if  start_match:
+        if start_date in date_list:
+            result = session.query(Measurement.tobs).filter(Measurement.date.between(start_date, end_date)).all()
+            session.close()
+            tmin = np.min(result)
+            tmax = np.max(result)
+            tavg = np.average(result)
+            start_date_list = {'TMIN':tmin, 'TAVG':tavg, 'TMAX':tmax}
+            return jsonify(start_date_list)
+        else:
+            return error_msg_2
+    else:
+        return error_msg_1
+
+
+@app.route('/api/v1.0/<start_date>/<end_date>')
+def temp_from_to_date(start_date, end_date):
+    start_match = re.match(r"\d{4}\W\d{2}\W\d{2}", start_date)
+    end_match = re.match(r"\d{4}\W\d{2}\W\d{2}", end_date)
+    session = Session(engine)
+    dates = session.query(Measurement.date).all()
+    blacklist = ['(', "'", ',', ')']
+    date_list = []
+    for date in dates:
+        new_string = ''
+        for char in date:
+            if char in blacklist:
+                new_string += ''
+            else:
+                new_string += char
+        date_list.append(new_string)
+    error_msg_1 = 'Invalid date input.\nPlease use YYYY-MM-DD format.'
+    error_msg_2 = 'Invalid date input.\nPlease input dates between 2010-01-01 and 2017-08-23'
+    if start_match:
+        if  end_match:
+            if start_date in date_list:
+                if end_date in date_list:
+                    result = session.query(Measurement.tobs).filter(Measurement.date.between(start_date, end_date)).all()
+                    session.close()
+                    tmin = np.min(result)
+                    tmax = np.max(result)
+                    tavg = np.average(result)
+                    date_list = {'TMIN':tmin, 'TAVG':tavg, 'TMAX':tmax}
+                    return jsonify(date_list)
+                else:
+                    return error_msg_2
+            else:
+                return error_msg_2
+        else:
+            return error_msg_1
+    else:
+        return error_msg_1
+
 
 if __name__ == '__main__':
     app.run(debug=True)
